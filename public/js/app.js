41a9184a30077f39b9b434dbec53fa434b07186f
Gond <akash.gond@sgdigital.com>
2021-05-25 12:56:46 +0530

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+console.log("Client side javascript file is loaded!");
+
+// fetch('http://api.weatherstack.com/current?access_key=85d4be2bfa1a77d49ae46298945856fa&query=boston').then(response =>{
+//     response.json().then((data) =>{
+//         console.log(data.location.country);
+//         console.log(data.current.weather_descriptions[0]);
+
+//     })
+// })
+
+const weatherform = document.querySelector("form");
+const search = document.querySelector("input");
+const message_1 = document.querySelector("#message-1");
+const message_2 = document.querySelector("#message-2");
+
+weatherform.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const location = search.value;
+  console.log("Button clicked " + location);
+
+  message_1.textContent = "Loading....";
+  message_2.textContent = "";
+
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          message_1.textContent = data.error;
+        }
+        message_1.textContent = data.location;
+        message_2.textContent = data.forecast;
+      });
+    }
+  );
+});
